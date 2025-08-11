40bf7cae4db03985f0f85662489a88034561cbe1
felipekouyoumdjian <fkouyoumdjian@mail.austral.edu.ar>
2025-08-11 12:57:20 -0300

    Add files via upload

--- exercise_math.py
+++ exercise_math.py
@@ -1,3 +1,20 @@
 def math():
     a = 57
     b = 7
+    
+    suma = a + b 
+    print(suma) # suma
+    resta = a - b
+    print(resta) # resta    
+    producto = a * b
+    print(producto) #producto
+    promedio = (a + b) / 2
+    print(promedio)
+    cociente = (a//b)
+    print(cociente)
+    resto = a%b
+    print(resto)
+    cociente_real = a/b
+    print(cociente_real)
+
+#math()
